3f86cc2c47d2f5f4253fd2bdc5854c6438fcb003
mr12tony <anton@mobius.team>
2026-08-17 00:48:53 +0300

    Windows manifest

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,19 @@
 fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = embed_resource::ResourceBuilder::new();
+        // Указываем манифест, требующий права администратора
+        res.set_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+</assembly>
+"#);
+        res.compile().unwrap();
+    }
     tauri_build::build()
 }
