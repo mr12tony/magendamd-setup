3c6325382f87129cc1a0a23f099350fc96d230d6
mr12tony <anton@mobius.team>
2026-08-18 16:16:27 +0300

    Upd ps1

--- src-tauri/resources/windows/apply-rustdesk-config.ps1
+++ src-tauri/resources/windows/apply-rustdesk-config.ps1
@@ -709,13 +709,22 @@ $service = Get-Service `
     -Name $ServiceName `
     -ErrorAction SilentlyContinue
 
+# if ($null -eq $service) {
+
+#     Write-Host ""
+#     Write-Host "ERROR: RustDesk service does not exist." -ForegroundColor Red
+#     Write-Host ""
+
+#     exit 1
+# }
+
 if ($null -eq $service) {
-
-    Write-Host ""
-    Write-Host "ERROR: RustDesk service does not exist." -ForegroundColor Red
-    Write-Host ""
-
-    exit 1
+    Write-Host "RustDesk service is not installed."
+    Write-Host "GUI remains running."
+}
+else {
+    Start-Service -Name $ServiceName
+    Write-Host "RustDesk service started."
 }
 
 
